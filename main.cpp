7638775e7f35291c395d3be517d08bd96574b12a
Jaiime10 <jcanaman@myuax.com>
2024-10-31 11:50:23 +0100

    Hola

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by canas on 31/10/2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by canas on 31/10/2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
